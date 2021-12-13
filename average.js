828c22ca8789d5d766e0961491bc66947bec65f9
prakash-cr7 <prakashkumarraj1@gmail.com>
2021-12-14 00:50:49 +0530

    refactor to check for threshold in error scenario

--- average.js
+++ average.js
@@ -1,14 +1,46 @@
-
 function average(numbers) {
-  let sum = 0
-  let count = 0
-  numbers.forEach(element => {
-    if(!isNaN(element)) {
-      sum+=element
-      count++
+    if (doesNaNSequenceExceeds(numbers) || doesNaNPercentExceeds(numbers)) {
+        return NaN
     }
-  });
-  return sum/count
+    let sum = 0
+    let count = 0
+    numbers.forEach(element => {
+        if (!isNaN(element)) {
+            sum += element
+            count++
+        }
+    });
+    return sum / count
 }
 
-module.exports = {average};
+// Max number of NaN data that can appear in the input array sequently
+const maxAllowedNaNSeq = 3
+
+// Max fraction of NaN data that can appear in the input array
+const maxAllowedNaNPercent = 0.2
+
+function doesNaNSequenceExceeds(numbers) {
+    let flag = false
+    for (let i = 0; i < numbers.length; i++) {
+        if (isNaN(numbers[i])) {
+            flag = true
+            for (let j = i + 1; j <= i + maxAllowedNaNSeq; j++) {
+                flag = flag && isNaN(numbers[j])
+            }
+        }
+        if (flag) return true
+    }
+    return false
+}
+
+function doesNaNPercentExceeds(numbers) {
+    let count = 0
+    numbers.forEach(element => {
+        if (isNaN(element)) {
+            count++
+        }
+    });
+    return count / numbers.length > maxAllowedNaNPercent
+}
+
+module.exports = { average, doesNaNSequenceExceeds, doesNaNPercentExceeds };
--- test/average.test.js
+++ test/average.test.js
@@ -1,21 +1,37 @@
-const {expect} = require('chai');
-const {average} = require('../average');
+const { expect } = require('chai');
+const { average, doesNaNSequenceExceeds, doesNaNPercentExceeds } = require('../average');
 
-it('computes average of a list of numbers', ()=> {
-  // floating point numbers cannot be compared for equality,
-  // hence allowing a delta tolerance
-  expect(average([1, 2, 3, 4])).to.be.approximately(2.5, 0.01);
+it('computes average of a list of numbers', () => {
+    // floating point numbers cannot be compared for equality,
+    // hence allowing a delta tolerance
+    expect(average([1, 2, 3, 4])).to.be.approximately(2.5, 0.01);
 });
 
-it('reports the average as NaN on an empty list', ()=> {
-  expect(average([])).to.be.NaN;
+it('reports the average as NaN on an empty list', () => {
+    expect(average([])).to.be.NaN;
 });
 
-it('ignores NaN in the input', ()=> {
-  expect(average([1, NaN, 2])).to.be.approximately(1.5, 0.01);
+it('ignores NaN in the input', () => {
+    expect(average([1, NaN, 2, 3, 4, 5])).to.be.approximately(3, 0.01);
 });
 
 // If input is an array of only NaN, the average is NaN
-it('reports the avarage as NaN on a list of NaN', ()=> {
-  expect(average([ NaN, NaN ])).to.be.NaN;
+it('reports the average as NaN on a list of NaN', () => {
+    expect(average([NaN, NaN])).to.be.NaN;
+})
+
+it('returns true if no of NaN in sequence exceeds the limit', () => {
+    expect(doesNaNSequenceExceeds([3, 4, NaN, NaN, NaN, NaN, 5, 7])).to.be.true
+})
+
+it('returns true if fraction of NaN in sequence exceeds the limit', () => {
+    expect(doesNaNPercentExceeds([9, 6, 5, NaN, 2, 5, 4, NaN, 5])).to.be.true
+})
+
+it('reports the average as NaN if NaN sequence in input exceeds limits', () => {
+    expect(average([1, NaN, NaN, NaN, NaN, 3, 5, 6, 6, 5, 4, 4, 2, 5, 6, 1, 5, 4, 2])).to.be.NaN;
+})
+
+it('reports the average as NaN if percentage of NaN values in input exceeds limits', () => {
+    expect(average([1, 3, 5, NaN, 2, 5, 4, NaN, 5])).to.be.NaN
 })
